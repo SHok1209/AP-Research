ef0c74211936938826112cd7c2a9217d394bb321
SHok1209 <Sho.kita.sho@gmail.com>
2025-03-19 07:19:16 +0000

    dfh

--- Test.py
+++ Test.py
@@ -4,7 +4,7 @@ import numpy as np
 
 
 
-allfile = pd.read_csv('AllCrypto.csv', parse_dates=['Date'])
+allfile = pd.read_csv('Crypto.csv', parse_dates=['Date'])
 
 
 
@@ -27,7 +27,131 @@ allfile["BitcoinPercentDif"] = ((allfile['Bitcoin High'] - allfile['Bitcoin Low'
 
 allfile["EthereumPercentDif"]= ((allfile['Ethereum High'] - allfile['Ethereum Low']) / allfile['Ethereum Low'] * 100)
 
+allfile["DogecoinPercentDif"] = ((allfile['Dogecoin High'] - allfile['Dogecoin Low']) / allfile['Dogecoin Low'] * 100)
+
+allfile["SolanaPercentDif"]= ((allfile['Solana High'] - allfile['Solana Low']) / allfile['Solana Low'] * 100)
+
+allfile["CardanoPercentDif"] = ((allfile['Cardano High'] - allfile['Cardano Low']) / allfile['Cardano Low'] * 100)
+
+allfile["BinancePercentDif"]= ((allfile['Binance High'] - allfile['Binance Low']) / allfile['Binance Low'] * 100)
+
+allfile["AvalanchePercentDif"]= ((allfile['Avalanche High'] - allfile['Avalanche Low']) / allfile['Avalanche Low'] * 100)
+
+allfile["MoneroPercentDif"]= ((allfile['Monero High'] - allfile['Monero Low']) / allfile['Monero Low'] * 100)
+
+allfile["LitecoinPercentDif"]= ((allfile['Litecoin High'] - allfile['Litecoin Low']) / allfile['Litecoin Low'] * 100)
+
+allfile["DashPercentDif"]= ((allfile['Dash High'] - allfile['Dash Low']) / allfile['Dash Low'] * 100)
 print(allfile['Date'])
+
+
+"BitcoinPercentDif", "EthereumPercentDif", 'TetherPercentDif', 'SolanaPercentDif', 'BinancePercentDif', 'CardanoPercentDif'
+
+allfile.plot(x = 'Date', y = ["BitcoinPercentDif", "EthereumPercentDif", 'DogecoinPercentDif', 'SolanaPercentDif', 'BinancePercentDif', 'CardanoPercentDif', 'AvalanchePercentDif', 'LitecoinPercentDif', 'MoneroPercentDif', 'DashPercentDif'])
+plt.title('crypto change ')
+plt.xlabel('Date')
+plt.xlim('2024-12-31', '2024-12-01')  
+plt.ylabel('Percent Difference')
+plt.show()
+plt.savefig('percentDEC.png')
+
+
+allfile.plot(x = 'Date', y = ["BitcoinPercentDif", "EthereumPercentDif", 'DogecoinPercentDif', 'SolanaPercentDif', 'BinancePercentDif', 'CardanoPercentDif', 'AvalanchePercentDif', 'LitecoinPercentDif', 'MoneroPercentDif', 'DashPercentDif'])
+plt.title('crypto change ')
+plt.xlabel('Date')
+plt.xlim('2024-11-30', '2024-11-01')  
+plt.ylabel('Percent Difference')
+plt.show()
+plt.savefig('percentNOV.png')
+
+
+allfile.plot(x = 'Date', y = ["BitcoinPercentDif", "EthereumPercentDif", 'DogecoinPercentDif', 'SolanaPercentDif', 'BinancePercentDif', 'CardanoPercentDif', 'AvalanchePercentDif', 'LitecoinPercentDif', 'MoneroPercentDif', 'DashPercentDif'])
+plt.title('crypto change ')
+plt.xlabel('Date')
+plt.xlim('2024-10-31', '2024-10-01')  
+plt.ylabel('Percent Difference')
+plt.show()
+plt.savefig('percentOCT.png')
+
+
+allfile.plot(x = 'Date', y = ["BitcoinPercentDif", "EthereumPercentDif", 'DogecoinPercentDif', 'SolanaPercentDif', 'BinancePercentDif', 'CardanoPercentDif', 'AvalanchePercentDif', 'LitecoinPercentDif', 'MoneroPercentDif', 'DashPercentDif'])
+plt.title('crypto change ')
+plt.xlabel('Date')
+plt.xlim('2024-09-30', '2024-09-01')  
+plt.ylabel('Percent Difference')
+plt.show()
+plt.savefig('percentSEP.png')
+
+allfile.plot(x = 'Date', y = ["BitcoinPercentDif", "EthereumPercentDif", 'DogecoinPercentDif', 'SolanaPercentDif', 'BinancePercentDif', 'CardanoPercentDif', 'AvalanchePercentDif', 'LitecoinPercentDif', 'MoneroPercentDif', 'DashPercentDif'])
+plt.title('crypto change ')
+plt.xlabel('Date')
+plt.xlim('2024-08-31', '2024-08-01')  
+plt.ylabel('Percent Difference')
+plt.show()
+plt.savefig('percentAUG.png')
+
+allfile.plot(x = 'Date', y = ["BitcoinPercentDif", "EthereumPercentDif", 'DogecoinPercentDif', 'SolanaPercentDif', 'BinancePercentDif', 'CardanoPercentDif', 'AvalanchePercentDif', 'LitecoinPercentDif', 'MoneroPercentDif', 'DashPercentDif'])
+plt.title('crypto change ')
+plt.xlabel('Date')
+plt.xlim('2024-07-31', '2024-07-01')  
+plt.ylabel('Percent Difference')
+plt.show()
+plt.savefig('percentJUL.png')
+
+allfile.plot(x = 'Date', y = ["BitcoinPercentDif", "EthereumPercentDif", 'DogecoinPercentDif', 'SolanaPercentDif', 'BinancePercentDif', 'CardanoPercentDif', 'AvalanchePercentDif', 'LitecoinPercentDif', 'MoneroPercentDif', 'DashPercentDif'])
+plt.title('crypto change ')
+plt.xlabel('Date')
+plt.xlim('2024-06-30', '2024-06-01')  
+plt.ylabel('Percent Difference')
+plt.show()
+plt.savefig('percentJUN.png')
+
+
+allfile.plot(x = 'Date', y = ["BitcoinPercentDif", "EthereumPercentDif", 'DogecoinPercentDif', 'SolanaPercentDif', 'BinancePercentDif', 'CardanoPercentDif', 'AvalanchePercentDif', 'LitecoinPercentDif', 'MoneroPercentDif', 'DashPercentDif'])
+plt.title('crypto change ')
+plt.xlabel('Date')
+plt.xlim('2024-05-31', '2024-05-01')  
+plt.ylabel('Percent Difference')
+plt.show()
+plt.savefig('percentMAY.png')
+
+allfile.plot(x = 'Date', y = ["BitcoinPercentDif", "EthereumPercentDif", 'DogecoinPercentDif', 'SolanaPercentDif', 'BinancePercentDif', 'CardanoPercentDif', 'AvalanchePercentDif', 'LitecoinPercentDif', 'MoneroPercentDif', 'DashPercentDif'])
+plt.title('crypto change ')
+plt.xlabel('Date')
+plt.xlim('2024-04-30', '2024-04-01')  
+plt.ylabel('Percent Difference')
+plt.show()
+plt.savefig('percentAPR.png')
+
+allfile.plot(x = 'Date', y = ["BitcoinPercentDif", "EthereumPercentDif", 'DogecoinPercentDif', 'SolanaPercentDif', 'BinancePercentDif', 'CardanoPercentDif', 'AvalanchePercentDif', 'LitecoinPercentDif', 'MoneroPercentDif', 'DashPercentDif'])
+plt.title('crypto change ')
+plt.xlabel('Date')
+plt.xlim('2024-03-31', '2024-03-01')  
+plt.ylabel('Percent Difference')
+plt.show()
+plt.savefig('percentMAR.png')
+
+allfile.plot(x = 'Date', y = ["BitcoinPercentDif", "EthereumPercentDif", 'DogecoinPercentDif', 'SolanaPercentDif', 'BinancePercentDif', 'CardanoPercentDif', 'AvalanchePercentDif', 'LitecoinPercentDif', 'MoneroPercentDif', 'DashPercentDif'])
+plt.title('crypto change ')
+plt.xlabel('Date')
+plt.xlim('2024-02-29', '2024-02-01')  
+plt.ylabel('Percent Difference')
+plt.show()
+plt.savefig('percentFEB.png')
+
+allfile.plot(x = 'Date', y = ["BitcoinPercentDif", "EthereumPercentDif", 'DogecoinPercentDif', 'SolanaPercentDif', 'BinancePercentDif', 'CardanoPercentDif', 'AvalanchePercentDif', 'LitecoinPercentDif', 'MoneroPercentDif', 'DashPercentDif'])
+plt.title('crypto change ')
+plt.xlabel('Date')
+plt.xlim('2024-01-31', '2024-01-01')  
+plt.ylabel('Percent Difference')
+plt.show()
+plt.savefig('percentJAN.png')
+
+
+
+
+
+
 
 
 
@@ -37,7 +161,7 @@ plt.xlabel('Date')
 plt.xlim('2024-12-31', '2024-10-01')  
 plt.ylabel('Percent Difference')
 plt.show()
-plt.savefig('percentDECOCT.png')
+plt.savefig('bitethpercentDECOCT.png')
 
 
 allfile.plot(x = 'Date', y = ["BitcoinPercentDif", "EthereumPercentDif"])
@@ -46,7 +170,7 @@ plt.xlabel('Date')
 plt.xlim('2024-09-30', '2024-07-01')  
 plt.ylabel('Percent Difference')
 plt.show()
-plt.savefig('percentSEPJUL.png')
+plt.savefig('bitethpercentSEPJUL.png')
 
 allfile.plot(x = 'Date', y = ["BitcoinPercentDif", "EthereumPercentDif"])
 plt.title('crypto change ')
@@ -54,7 +178,7 @@ plt.xlabel('Date')
 plt.xlim('2024-06-30', '2024-04-01')  
 plt.ylabel('Percent Difference')
 plt.show()
-plt.savefig('percentMAYAPR.png')
+plt.savefig('bitethpercentMAYAPR.png')
 
 allfile.plot(x = 'Date', y = ["BitcoinPercentDif", "EthereumPercentDif"])
 plt.title('crypto change ')
@@ -62,10 +186,18 @@ plt.xlabel('Date')
 plt.xlim('2024-03-30', '2024-01-01')  
 plt.ylabel('Percent Difference')
 plt.show()
-plt.savefig('percentMARJAN.png')
+plt.savefig('bitethpercentMARJAN.png')
 
 print('The average Bitcoin price is', sum(allfile['Bitcoin Open']) / len(allfile['Bitcoin Open']))
 print('The average Ethereum price is',sum(allfile['Ethereum Open']) / len(allfile['Ethereum Open']))
+
+
+
+
+
+
+
+
 
 
 
@@ -77,6 +209,97 @@ print(Bithigh)
 top_indices = allfile['BitcoinPercentDif'].nlargest(10).index
 top_data = allfile.loc[top_indices,['BitcoinPercentDif', 'Date']]
 
+#########################################################################
+
+allfile['high']= sorted(allfile['BitcoinPercentDif'], reverse=True)
+Bithigh = allfile['Bithigh'][:10]
+print(Bithigh)
+
+# Get the top 10 largest values and their indices
+top_indices = allfile['BitcoinPercentDif'].nlargest(10).index
+top_data = allfile.loc[top_indices,['BitcoinPercentDif', 'Date']]
+
+#########################################################################
+
+allfile['Bithigh']= sorted(allfile['BitcoinPercentDif'], reverse=True)
+Bithigh = allfile['Bithigh'][:10]
+print(Bithigh)
+
+# Get the top 10 largest values and their indices
+top_indices = allfile['BitcoinPercentDif'].nlargest(10).index
+top_data = allfile.loc[top_indices,['BitcoinPercentDif', 'Date']]
+
+#########################################################################
+
+allfile['Bithigh']= sorted(allfile['BitcoinPercentDif'], reverse=True)
+Bithigh = allfile['Bithigh'][:10]
+print(Bithigh)
+
+# Get the top 10 largest values and their indices
+top_indices = allfile['BitcoinPercentDif'].nlargest(10).index
+top_data = allfile.loc[top_indices,['BitcoinPercentDif', 'Date']]
+
+#########################################################################
+
+allfile['Bithigh']= sorted(allfile['BitcoinPercentDif'], reverse=True)
+Bithigh = allfile['Bithigh'][:10]
+print(Bithigh)
+
+# Get the top 10 largest values and their indices
+top_indices = allfile['BitcoinPercentDif'].nlargest(10).index
+top_data = allfile.loc[top_indices,['BitcoinPercentDif', 'Date']]
+
+#########################################################################
+
+allfile['Bithigh']= sorted(allfile['BitcoinPercentDif'], reverse=True)
+Bithigh = allfile['Bithigh'][:10]
+print(Bithigh)
+
+# Get the top 10 largest values and their indices
+top_indices = allfile['BitcoinPercentDif'].nlargest(10).index
+top_data = allfile.loc[top_indices,['BitcoinPercentDif', 'Date']]
+
+#########################################################################
+
+allfile['Bithigh']= sorted(allfile['BitcoinPercentDif'], reverse=True)
+Bithigh = allfile['Bithigh'][:10]
+print(Bithigh)
+
+# Get the top 10 largest values and their indices
+top_indices = allfile['BitcoinPercentDif'].nlargest(10).index
+top_data = allfile.loc[top_indices,['BitcoinPercentDif', 'Date']]
+
+#########################################################################
+
+allfile['Bithigh']= sorted(allfile['BitcoinPercentDif'], reverse=True)
+Bithigh = allfile['Bithigh'][:10]
+print(Bithigh)
+
+# Get the top 10 largest values and their indices
+top_indices = allfile['BitcoinPercentDif'].nlargest(10).index
+top_data = allfile.loc[top_indices,['BitcoinPercentDif', 'Date']]
+
+#########################################################################
+
+allfile['Bithigh']= sorted(allfile['BitcoinPercentDif'], reverse=True)
+Bithigh = allfile['Bithigh'][:10]
+print(Bithigh)
+
+# Get the top 10 largest values and their indices
+top_indices = allfile['BitcoinPercentDif'].nlargest(10).index
+top_data = allfile.loc[top_indices,['BitcoinPercentDif', 'Date']]
+
+#########################################################################
+
+allfile['Bithigh']= sorted(allfile['BitcoinPercentDif'], reverse=True)
+Bithigh = allfile['Bithigh'][:10]
+print(Bithigh)
+
+# Get the top 10 largest values and their indices
+top_indices = allfile['BitcoinPercentDif'].nlargest(10).index
+top_data = allfile.loc[top_indices,['BitcoinPercentDif', 'Date']]
+
+#########################################################################
 plt.figure(figsize=(10, 5))
 plt.bar(top_data['Date'], top_data['BitcoinPercentDif'], color='black')
 
